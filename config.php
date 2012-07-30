1fdd1802fe8bb870a926c5fbb70f7f89e8c697ef
Zachary <zbloomq@live.com>
2012-07-29 20:44:30 -0400

    initial commit

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+// WebTools configuration file
+// General WebTools configuration
+	$config['ratelimit'] = 5; // The amount of time, in seconds, before an IP can make another request.
+	$config['showerrors'] = TRUE; // Should the script output errors to the browser if they occur? Set to FALSE for production environments, TRUE for development environments.
+	$config['dieonerror'] = FALSE; // Should the script die() if ANY error or warning occurs?
+	$config['limitfolder'] = './ratelimit/'; // The folder in which rate limit information is stored.
+	$config['template'] = './template.html'; // The location of the page template.
+	$config['homepage'] = 'home'; // The name of the module used to handle requests that don't specify a page.
+	$config['404page'] = '404'; // The name of the page used to handle requests to non-existing pages.
+// "whois" page configuration
+	$config['serverlist'] = './whois-servers.txt'; // The location of the whois-servers.txt file, relative to index.php.
+	$config['maxredirs'] = 1; // The maximum number of WHOIS servers to bounce through. Recommended value is 1.
---
+++ index.php
@@ -0,0 +1,55 @@
+<?php
+// This is the code used to display individual pages within the site. For page code, please see the
+// files in the /pages/ directory. If you'd like to edit the layout of the site, you should see
+// template.html - there's very little that's relevant to the site layout within this file.
+// ------------------------------------------------------------------------------------------------
+set_error_handler('webtools_error_handler');
+$default_tz = @date_default_timezone_get(); // get PHP's default timezone...
+date_default_timezone_set($default_tz); // ...and reset it so we don't get silly errors when we use time functions
+define('IN_WEBTOOLS',TRUE); // check this constant in your pages to see if the user is visiting via WebTools or via other less regulated means
+define('START_TIME',microtime(TRUE)); // enables us to get loadtime stats almost down to the millisecond
+ini_set('error_reporting',E_ALL|E_STRICT);
+$config = array();
+$page_title = '';
+require('./config.php');
+// Load up the template
+$template = @file_get_contents($config['template']); // @ it so no ugly errors are seen if the script fails
+if (!$template) {
+	trigger_error('Unable to load the template. Please refer to the documentation for more details.',E_USER_ERROR);
+	die();
+}
+if (empty($_GET['page'])) {
+	$page = $config['homepage'];
+} else {
+	$page = explode('.',$_GET['page']);
+	$page = end($page);
+	$page = basename($page);
+}
+$self_link = $_SERVER['PHP_SELF'] . '?page=' . $page;
+if (file_exists('./pages/' . $page . '.php')) {
+	ob_start(); // begin output buffering so we can put the output of a page into a variable for templating
+	require_once('./pages/' . $page . '.php');
+	$content = ob_get_contents(); // put the output into a string variable
+	ob_end_clean(); // kill the output buffer so we can output stuff, this time for real
+	$template = str_replace(array('%content%','%generationtime%','%year%','%pagetitle%','%page%','%basehref%','%pagehref%'), // array of strings to replace
+		array($content,round(microtime(TRUE)-START_TIME,6),date('Y'),$page_title,$page,$_SERVER['PHP_SELF'],$self_link), // stuff to replace the strings with
+		$template);
+	echo $template;
+} else {
+	header('Location: ' . $_SERVER['PHP_SELF'] . '?page=' . $config['404page']); 
+}
+function webtools_error_handler($errno,$errstr,$errfile,$errline,$errcontext) {
+	global $config;
+	if (error_reporting()!==0) { // if they were using @, let's not show an error
+		if ($config['showerrors']) {
+			webtools_trigger_user_error('Error (' . $errno . '): ' . $errstr . ' (' . basename($errfile) . ', line ' . $errline . ')');
+		}
+		if ($config['dieonerror']) {
+			die();
+		}
+	}
+	return TRUE;
+}
+function webtools_trigger_user_error($text,$level='error') { // $level is one of 'error','warning','info' - ranging from most serious to least
+	echo '<div class="alert alert-' . $level . '">' . $text . '</div>';
+}
